dec5d16c1d4ca15bb95bc1a536ed274a36c21aae
Rafael Amico <dexter.amico@gmail.com>
2022-09-30 20:58:02 -0300

    Commit inicial

---
+++ readme.md
@@ -0,0 +1,5 @@
+# **SELETOR DE URL DA CLOUD9 PARA GA**
+
+**OBJETIVO**
+
+Este pequeno código tem a finalidade de facilitar a seleção das URLs de logs dos dispositivos da GreenAnt nos servidores da Cloud9, providenciando uma pequena interface web onde é escolhido o tipo de log e fornecido o MAC, retornando em uma nova janela a página de logs selecionada do respectivo MAC.
---
+++ script.js
@@ -0,0 +1,46 @@
+/*
+https://clodu9.greenant.com.br/log/MAC
+https://clodu9.greenant.com.br/meter/MAC
+https://clodu9.greenant.com.br/api/last/logs/MAC
+https://clodu9.greenant.com.br/api/last/measurements/MAC 
+*/
+
+function pegadados() {
+    var formDados = new FormData(document.querySelector('form'));
+    var modo = formDados.get('selector');
+    var mac = formDados.get('mac_address');
+
+    if (modo === "all_logs") {
+        modo = "https://clodu9.greenant.com.br/log/";
+    } else if (modo === "all_meter") {
+        modo = "https://clodu9.greenant.com.br/meter/";
+    } else if (modo === "last_logs") {
+        modo = "https://clodu9.greenant.com.br/api/last/logs/";
+    } else if (modo === "last_meter") {
+        modo = "https://clodu9.greenant.com.br/api/last/measurements/";
+    } else {
+        // error handling
+    };
+    endereco = modo + mac;
+    window.open(endereco);
+}
+
+/*
+function seletor(modo, mac) {
+    if (modo === "all_logs") {
+        modo = "https://clodu9.greenant.com.br/log/";
+    } else if (modo === "all_meter") {
+        modo = "https://clodu9.greenant.com.br/meter/";
+    } else if (modo === "last_logs") {
+        modo = "https://clodu9.greenant.com.br/api/last/logs/";
+    } else if (modo === "last_meter") {
+        modo = "https://clodu9.greenant.com.br/api/last/measurements/";
+    } else {
+        // error handling
+    };
+    endereco = modo + mac;
+    return endereco;
+}
+*/
+
+//console.log(seletor(modo, mac));
