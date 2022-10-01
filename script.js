7cc8f729f09e0080246fa52477c2a6e771610dea
Rafael Amico <dexter.amico@gmail.com>
2022-09-30 21:17:26 -0300

    Ajustes ortograficos e adicionada página de erro

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 /*
-https://clodu9.greenant.com.br/log/MAC
-https://clodu9.greenant.com.br/meter/MAC
-https://clodu9.greenant.com.br/api/last/logs/MAC
-https://clodu9.greenant.com.br/api/last/measurements/MAC 
+https://cloud9.greenant.com.br/log/MAC
+https://cloud9.greenant.com.br/meter/MAC
+https://cloud9.greenant.com.br/api/last/logs/MAC
+https://cloud9.greenant.com.br/api/last/measurements/MAC 
 */
 
 function pegadados() {
     var formDados = new FormData(document.querySelector('form'));
     var modo = formDados.get('selector');
     var mac = formDados.get('mac_address');
+
+    if (mac.length != 12) {
+        window.open("error.html");
+    }
 
     if (modo === "all_logs") {
         modo = "https://cloud9.greenant.com.br/log/";
@@ -19,28 +23,8 @@ function pegadados() {
     } else if (modo === "last_meter") {
         modo = "https://cloud9.greenant.com.br/api/last/measurements/";
     } else {
-        // error handling
+        window.open("error.html");
     };
     endereco = modo + mac;
     window.open(endereco);
-}
-
-/*
-function seletor(modo, mac) {
-    if (modo === "all_logs") {
-        modo = "https://clodu9.greenant.com.br/log/";
-    } else if (modo === "all_meter") {
-        modo = "https://clodu9.greenant.com.br/meter/";
-    } else if (modo === "last_logs") {
-        modo = "https://clodu9.greenant.com.br/api/last/logs/";
-    } else if (modo === "last_meter") {
-        modo = "https://clodu9.greenant.com.br/api/last/measurements/";
-    } else {
-        // error handling
-    };
-    endereco = modo + mac;
-    return endereco;
-}
-*/
-
-//console.log(seletor(modo, mac));
+};
