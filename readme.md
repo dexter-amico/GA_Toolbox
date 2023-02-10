9e3f80fd481a24aafb0b9e4bf2dc719fb2bd9669
Rafael Amico <dexter.amico@gmail.com>
2023-02-10 13:36:45 -0300

    Versão 0.2

--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
-# **SELETOR DE URL DA CLOUD9 PARA GA**
+# **CANIVETE SUÍCO DO ESTAGIÁRIO v0.2**
 
-**OBJETIVO**
+## **OBJETIVO**
 
-Este pequeno código tem a finalidade de facilitar a seleção das URLs de logs dos dispositivos da GreenAnt nos servidores da Cloud9, providenciando uma pequena interface web onde é escolhido o tipo de log e fornecido o MAC, retornando em uma nova janela a página de logs selecionada do respectivo MAC.
+Este pequeno código tem a finalidade de facilitar o dia-a-dia dos estagiários da GreenAnt, no momento temos duas ferramentas incluídas aqui:
+
+- Seleção das URLs de logs dos dispositivos da GreenAnt nos servidores da Cloud9, providenciando uma pequena interface web onde é escolhido o tipo de log e fornecido o MAC, retornando em uma nova janela a página de logs selecionada do respectivo MAC.
+- Conversor de caixa alta para caixa baixa dos MACs, útil para inserção de dados na ferramente do Park Management que aceita apenas valores de MAC em caixa baixa.
+
+---
+
+## **ATUALIZAÇÕES v0.2**
+
+- Adicionada a funcionalidade do conversor para caixa baixa, útil no momento de utilizar o Park Management, o qual só permite endereços MAC em caixa baixa.
+- Separadas em funções distintas a validação do MAC e a criação da URL do Cloud9
+- Agora a função de validação de MAC faz a remoção de espaços em branco antes e depois do MAC, não retornando erro para essa condição.
--- script.js
+++ script.js
@@ -5,24 +5,25 @@ https://cloud9.greenant.com.br/api/last/logs/MAC
 https://cloud9.greenant.com.br/api/last/measurements/MAC 
 */
 
-function pegadados() {
-    var formDados = new FormData(document.querySelector('form'));
-    var modo = formDados.get('selector');
-    var mac = formDados.get('mac_address');
-    
-    // Verificando se o MAC é válido
-    mac = mac.toUpperCase();
-    console.log(mac, mac.length);
+function validaMac(mac) {
+    //Valida o endereço MAC
+    mac = mac.toUpperCase().trim();
+    //console.log(mac, mac.length);
     if (mac.length != 12) {
-        console.log("erro de tamanho");
+        //console.log("erro de tamanho");
         window.open("error.html");
         return -1;
     } else if (!mac.match(/([A-F0-9]{12})/)) {
-        console.log("erro de simbolo");
+        //console.log("erro de simbolo");
         window.open("error.html");
         return -1;
+    } else {
+        return mac;
     };
+};
 
+function seletorCloud9 (mac, modo) {
+    //Retorna a URL do canal do cloud9 escolhido
     if (modo === "all_logs") {
         modo = "https://cloud9.greenant.com.br/log/";
     } else if (modo === "all_meter") {
@@ -32,10 +33,44 @@ function pegadados() {
     } else if (modo === "last_meter") {
         modo = "https://cloud9.greenant.com.br/api/last/measurements/";
     } else {
-        console.log("erro de seleção");
+        //console.log("erro de seleção");
         window.open("error.html");
         return -1;
     };
-    endereco = modo + mac;
-    window.open(endereco);
+    return endereco = modo + mac;
+}
+
+
+function pegadados() {
+    var formDados = new FormData(document.querySelector('form'));
+    var modo = formDados.get('selector');
+    var mac = formDados.get('mac_address');
+    
+    // Verificando se o MAC é válido
+    var macValido = validaMac(mac);
+    
+    if (macValido === -1) {
+        return -1;
+    };
+    
+    // Retornando a URL do Cloud9
+    var endereco = seletorCloud9 (macValido, modo);
+
+    if (endereco !== -1) {
+        window.open(endereco);
+        return 0;
+    } else {
+        return -1;
+    };
+};
+
+function caixaBaixa () {
+    var caixaAlta = document.getElementById('mac_caixa_alta').value;
+    caixaAlta = caixaAlta.trim();
+    // console.log(caixaAlta);
+    
+    var caixaBaixa = caixaAlta.toLowerCase();
+    // console.log(caixaBaixa);
+
+    return document.getElementById('caixa_baixa').innerHTML = caixaBaixa;
 };
